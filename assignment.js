5fec5fbe34e7f99bb48095e52cc7fc06a9719a61
Md. Ishrak Al Hasib Syeam <ishrakalhasib217@gmail.com>
2021-01-21 23:35:39 +0600

    created a budget calculator

--- assignment.js
+++ assignment.js
@@ -4,11 +4,26 @@
 function kilometerToMeter(kilometer) {
     if (kilometer < 0) {
         return "ERROR! Please Enter a positive number. Distance can not be a negative number.";
-    }
+    } // used if else method to check whether the distance is in negative number or not
     else {
         const meter = 1000;
         var meterResult = kilometer * meter;
         return meterResult;
+    }
+}
+
+// budget calculator
+function budgetCalculator(clockNum, phoneNum, laptopNum) {
+    if (clockNum < 0 || phoneNum < 0 || laptopNum < 0) {
+        return "ERROR! Please Enter a positive number. Product can not be a negative number.";
+    } // used if else method to check whether the products are in negative number or not
+    else {
+        var clocksCost = clockNum * 50;
+        var phonesCost = phoneNum * 100;
+        var laptopsCost = laptopNum * 500;
+
+        var totalCost = clocksCost + phonesCost + laptopsCost;
+        return totalCost;
     }
 }
 
